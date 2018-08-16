80ce0495e8b902c62c6b773c48dcb558f952557f
Adam Yab <ahmed.harazi22@gmail.com>
2018-08-16 18:01:33 -0400

    all the files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Friend-Finder
+# FriendFinder
+Friend Finder Overview A friend-matching app.  
+Users answer questions about themselves and the closest match of who they're compatible friends are. 
+View Demo Built with 
+
+
+Ahmed Alharazi
---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+//Friend Finder Survey (DATA)
+//=================================================================
+var friends = [{
+        name: "Mark ",
+        photo: "https://cdn.vox-cdn.com/thumbor/AwDrzvvfwN7JJu-XkW5aXLUmEJM=/0x0:640x480/920x613/filters:focal(212x108:314x210):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/60412901/Mark_Zuckerberg.0.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Obama",
+        photo: "https://pbs.twimg.com/profile_images/822547732376207360/5g0FC8XX_400x400.jpg",
+        scores: [
+            "1",
+            "1",
+            "4",
+            "4",
+            "5",
+            "5",
+            "3",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Hillary Clinton",
+        photo: "http://thepoliticalcarnival.net/2012/12/15/hillary-clinton-faints-has-concussion/hillary-rodham-clinton/",
+        scores: [
+            "1",
+            "1",
+            "4",
+            "4",
+            "5",
+            "5",
+            "3",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+]
+
+module.exports = friends;
